c730288663a1bf0c4813439a1d3ec59a83ab4669
aidar <rememberlig@gmail.com>
2019-03-03 19:18:47 +0300

    build folder added to repo

---
+++ build/precache-manifest.6c178279f0a3047db80e45d70060b24b.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = [
+  {
+    "revision": "f6c6f6c8cb7784254ad00056f6fbd74e",
+    "url": "/zr/static/media/fa-regular-400.f6c6f6c8.ttf"
+  },
+  {
+    "revision": "9800b8894d927409dcbe",
+    "url": "/zr/static/css/main.6f89c5c9.chunk.css"
+  },
+  {
+    "revision": "82905d8d1c06969df11c8c378e9bdd4c",
+    "url": "/zr/static/media/fa-solid-900.82905d8d.svg"
+  },
+  {
+    "revision": "a76d51fdb22041489762",
+    "url": "/zr/static/js/1.a76d51fd.chunk.js"
+  },
+  {
+    "revision": "823ad094b28e9e405cb3",
+    "url": "/zr/static/js/runtime~main.823ad094.js"
+  },
+  {
+    "revision": "f3d805d81a659df77fb3e7b44ec9d23a",
+    "url": "/zr/static/media/car1.f3d805d8.jpg"
+  },
+  {
+    "revision": "0ba8aa155562a752ebd1558098489985",
+    "url": "/zr/static/media/car2.0ba8aa15.jpg"
+  },
+  {
+    "revision": "0cc3a6bf8d056424401b3a1f97d05ba4",
+    "url": "/zr/static/media/car3.0cc3a6bf.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/zr/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "65779ebcc35604a25c2ba77309c5b8af",
+    "url": "/zr/static/media/fa-regular-400.65779ebc.woff2"
+  },
+  {
+    "revision": "5dd3976cb5d61e2e561f2a46b916f377",
+    "url": "/zr/static/media/fa-regular-400.5dd3976c.woff"
+  },
+  {
+    "revision": "414ff5daad323a1c47c5177d4bd29674",
+    "url": "/zr/static/media/fa-regular-400.414ff5da.eot"
+  },
+  {
+    "revision": "9800b8894d927409dcbe",
+    "url": "/zr/static/js/main.9800b889.chunk.js"
+  },
+  {
+    "revision": "4b115e1153a9ea339d6a0bb284cc8ed3",
+    "url": "/zr/static/media/fa-brands-400.4b115e11.woff2"
+  },
+  {
+    "revision": "462806316fea535a6a57651bc2b000b0",
+    "url": "/zr/static/media/fa-solid-900.46280631.woff2"
+  },
+  {
+    "revision": "b90365bccdabd68c6c03902b4b141f09",
+    "url": "/zr/static/media/fa-brands-400.b90365bc.woff"
+  },
+  {
+    "revision": "61969d433bf265b9717a6c357a1e04e4",
+    "url": "/zr/static/media/fa-solid-900.61969d43.woff"
+  },
+  {
+    "revision": "d9d17590c975aad1be0ddab673f9c769",
+    "url": "/zr/static/media/fa-brands-400.d9d17590.eot"
+  },
+  {
+    "revision": "c39278f7abfc798a241551194f55e29f",
+    "url": "/zr/static/media/fa-brands-400.c39278f7.ttf"
+  },
+  {
+    "revision": "e7e957c87c454bccaa3bf9fdaa6874f8",
+    "url": "/zr/static/media/fa-regular-400.e7e957c8.svg"
+  },
+  {
+    "revision": "b5596f4d339f99e3d69bc41be78db962",
+    "url": "/zr/static/media/fa-solid-900.b5596f4d.eot"
+  },
+  {
+    "revision": "b70cea0339374107969eb53e5b1f603f",
+    "url": "/zr/static/media/fa-solid-900.b70cea03.ttf"
+  },
+  {
+    "revision": "80533988ff5fecd5be26557d08ce8237",
+    "url": "/zr/static/media/fa-brands-400.80533988.svg"
+  },
+  {
+    "revision": "a76d51fdb22041489762",
+    "url": "/zr/static/css/1.528ebc80.chunk.css"
+  },
+  {
+    "revision": "751afb0e4a91bbd836043d25ce8fa26a",
+    "url": "/zr/index.html"
+  }
+];
